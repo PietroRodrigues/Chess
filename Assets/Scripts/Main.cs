9fdeb4a040adb9d9ac04065e295d0f6273e8167b
Pietro Rodrigues <pietrorodrigues.dev@gmail.com>
2021-07-16 01:46:42 -0300

    Programando 3

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,24 +6,65 @@ public class Main : MonoBehaviour
 {
     
 
-    [SerializeField] Transform casaTab;
-    public Mapeamento mapTotal = new Mapeamento();
-    public PecasMap mapPecas = new PecasMap();
-    Dictionary<string,Vector2> casas = new Dictionary<string, Vector2>();
-    Peca[] peca;
+  public Transform casaTab;
+  public Mapeamento mapTotal = new Mapeamento();
+  public PecasMap mapPecas = new PecasMap();
+  Dictionary<string,Vector2> casas = new Dictionary<string, Vector2>();
+  Peca[] peca;    
+  RaycastHit hit;
+  [SerializeField] Peca pecaSelect;
 
-    void Start()
-    {
-      peca = FindObjectsOfType<Peca>();
-      casas = mapTotal.Mapear(casaTab);
-      mapPecas.MapearPecas(casas,peca,casaTab);
-    }
+  void Start()
+  {
+    peca = FindObjectsOfType<Peca>();
+    casas = mapTotal.Mapear(casaTab);
+    mapPecas.MapearPecas(casas,peca,casaTab);
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-      if(Input.GetKeyDown(KeyCode.Space))
-        mapPecas.attMapPorJogada(casas,peca,casaTab);
+  void Update()
+  {
+      MovimentoPecasInput();
+
+  }
+
+  void MovimentoPecasInput(){
+    
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+          
+    bool too = Physics.Raycast(ray,out hit);
+
+    if(too){
+    
+      if(Input.GetMouseButtonDown(0)){       
+
+        bool mover = false;
+
+        for(int i = 0;i < peca.Length;i++){
+                    
+          if(peca[i].casaAtual == hit.collider.gameObject.name){
+
+            if(pecaSelect == null || peca[i].lado == pecaSelect.lado){
+              pecaSelect = peca[i];
+              mover = false;
+              i = peca.Length;
+            }
+
+          }else{
+            mover = true;
+          }                  
+        }
+
+        if(mover && pecaSelect != null){
+          pecaSelect.casaSelecionada = hit.collider.gameObject.name;
+          mapPecas.attMapPorJogada(casas,peca,casaTab);
+          pecaSelect.movimentada = true;
+          pecaSelect = null;
+
+        }                                        
+      
+      }
+
     }
+  }
 
 }
---
+++ Assets/Scripts/Peao.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peao 
+{
+    Dictionary<string,Vector2> Cordenadas;
+    Peca[] pecas;
+    Transform tabuleiro;
+    
+    Peao(Dictionary<string,Vector2> casas,Peca[] peca,Transform casaTab){
+               
+
+    }
+
+
+}
